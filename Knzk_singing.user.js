4e1f7703a7e10927c7a79132b5ac2278bce99193
ねじりわさび(yuzu) <git-yuzu@svk.jp>
2017-09-02 14:46:32 +0900

    v1.1.3 はみ出ないように修正

--- Knzk_singing.user.js
+++ Knzk_singing.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Knzk_singing
 // @namespace    https://github.com/yuzulabo
-// @version      1.1.2
+// @version      1.1.3
 // @description  おにいさんがうたいます
 // @author       neziri_wasabi (yuzu_1203)
-// @match        https://knzkoniisan.m.to/web/*
-// @match        https://friends.nico/web/*
+// @match        https://knzkoniisan.m.to/*
+// @match        https://friends.nico/*
 // @license       MIT License
 // ==/UserScript==
 
@@ -16,10 +16,12 @@
     audio_src[1] = "https://s3.mqube.net/t/files/item/file/201708/20170804623468/766aac33.wav";
 
     var audio_rand = Math.floor(Math.random()*audio_src.length);
-    var nav = document.getElementsByClassName('compose-form');
+    var nav = document.getElementsByClassName('drawer__inner');
 
     var audio_elem = document.createElement('audio');
     audio_elem.src = audio_src[audio_rand];
+    audio_elem.setAttribute('style', 'width: 100%;');
+    audio_elem.setAttribute('controlslist', 'nodownload');
     audio_elem.setAttribute('controls', '1');
     audio_elem.setAttribute('autoplay', '1');
     audio_elem.setAttribute('loop', '1');
